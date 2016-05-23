926b62d6d07cdc3f3e4c096c10b33623e7173ffa
zxiu-bonofa <z.xiu@bonofa.com>
2016-05-23 10:42:42 +0200

    first step for devise, need test add bootstrap

---
+++ app/controllers/system_controller.rb
@@ -0,0 +1,5 @@
+class SystemController < ApplicationController
+
+  before_action :authenticate_user!
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root :to => "system#index"
+
+  get 'system' => 'system#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

bc417d3f1ddfba76ffeb1683693b0834307a4b06
zxiu-bonofa <z.xiu@bonofa.com>
2016-05-24 13:40:08 +0200

    generate model study info, going to migrate

---
+++ db/migrate/20160524113151_create_study_infos.rb
@@ -0,0 +1,15 @@
+class CreateStudyInfos < ActiveRecord::Migration
+  def change
+    create_table :study_infos do |t|
+      t.string :insititude
+      t.string :subject
+      t.date   :begin
+      t.date   :end
+      t.string :graduation
+      t.string :score
+      t.references :user, index: true, foreign_key: true
+      
+      t.timestamps null: false
+    end
+  end
+end
---
+++ test/models/study_info_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StudyInfoTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

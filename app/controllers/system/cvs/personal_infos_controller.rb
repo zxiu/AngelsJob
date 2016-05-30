5c283fb9ed9d21eec6b53e66e539216d1f83b68e
zxiu <zxiu2015@gmail.com>
2016-05-30 17:01:59 +0200

    add address to personal info

--- app/controllers/system/cvs/personal_infos_controller.rb
+++ app/controllers/system/cvs/personal_infos_controller.rb
@@ -12,7 +12,7 @@ class System::Cvs::PersonalInfosController < System::CvsController
   end
 
   def update
-    if @personal_info.update(personal_info_params)
+    if @personal_info.update(personal_info_params) && @personal_info.address.update(address_params)
       redirect_to action: :index, notice: 'Personal info was successfully updated.'
     else
       render :index
@@ -25,12 +25,19 @@ class System::Cvs::PersonalInfosController < System::CvsController
         current_user.personal_info = PersonalInfo.create
         current_user.personal_info.email = current_user.email
       end
+      if current_user.personal_info.address.blank?
+         current_user.personal_info.address = Address.new
+      end
       @personal_info = current_user.personal_info
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def personal_info_params
       params.require(:personal_info).permit(:first_name, :last_name, :email, :tel, :mobile, :birthday, :gender, :avatar, :remote_avatar_url)
+    end
+
+    def address_params
+      params.require(:address).permit(:street, :house_no, :city, :postal_code, :country)
     end
 
 end
--- app/models/personal_info.rb
+++ app/models/personal_info.rb
@@ -2,6 +2,8 @@ class PersonalInfo < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
 
   belongs_to :user
+  has_one :address, as: :locatable, autosave: true
+
   enum gender: [:female, :male]
 
   def full_name
